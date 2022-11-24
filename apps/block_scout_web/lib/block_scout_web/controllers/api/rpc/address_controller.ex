6c19f7dc0d83970c6048f1eb0875e540b779c1e5
hundredark <hundredark@gmail.com>
2022-11-24 15:18:22 +0800

    Fix format

--- apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/address_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/address_controller.ex
@@ -234,6 +234,7 @@ defmodule BlockScoutWeb.API.RPC.AddressController do
       user_assets_with_balance = Enum.map(token_list, fn x -> to_string(x.contract_address_hash) end)
 
       total_assets = Chain.list_top_tokens("")
+
       default_assets =
         Enum.filter(total_assets, fn x ->
           contract = to_string(x.contract_address_hash)
