966f85b934a1ede79db9d4af41b91205ad41c60d
hundredark <hundredark@gmail.com>
2022-12-12 20:01:15 +0800

    Slight fixes

--- apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/token_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/token_controller.ex
@@ -1,8 +1,8 @@
 defmodule BlockScoutWeb.API.RPC.TokenController do
   use BlockScoutWeb, :controller
- 
-  alias Ecto.UUID
+
   alias BlockScoutWeb.API.RPC.Helpers
+  alias Ecto.UUID
   alias Explorer.{Chain, PagingOptions}
   alias Explorer.Chain.Hash.Address
 
@@ -166,29 +166,30 @@ defmodule BlockScoutWeb.API.RPC.TokenController do
   def batchsearch(conn, params) do
     with {:index_type, {:ok, index_type}} <- fetch_type(params),
          {:indices, {:ok, indices}} <- fetch_indices(params),
-         {:user_address, user} <- fetch_user(params), 
+         {:user_address, user} <- fetch_user(params),
          {:ok, index_list} <- is_valid_type_and_indices(index_type, indices),
          {:ok, user_address_hash} <- is_valid_user_address(user) do
       tokens = Chain.search_batch_tokens(index_type, index_list, user_address_hash)
 
-      asset_list = Enum.map(tokens, fn t ->
-        info = Chain.token_add_price_and_chain_info(t)
-        Map.merge(t, info)
-      end)
+      asset_list =
+        Enum.map(tokens, fn t ->
+          info = Chain.token_add_price_and_chain_info(t)
+          Map.merge(t, info)
+        end)
 
       render(conn, :batchsearch, %{list: asset_list})
     else
-      {:index_type, :error} -> 
+      {:index_type, :error} ->
         conn
         |> put_status(200)
         |> render(:error, error: "Type is required.")
 
-      {:indices, :error} ->    
+      {:indices, :error} ->
         conn
         |> put_status(200)
         |> render(:error, error: "Indices is required.")
 
-      {:error, :invalid_type} -> 
+      {:error, :invalid_type} ->
         conn
         |> put_status(200)
         |> render(:error, error: "Invalid type. Type should be in ['contract', 'uuid'].")
@@ -198,49 +199,47 @@ defmodule BlockScoutWeb.API.RPC.TokenController do
         |> put_status(200)
         |> render(:error, error: "Invalid indices, indices should be either contract addresses or uuids split by ','.")
 
-      {:error, :invalid_user} -> 
+      {:error, :invalid_user} ->
         conn
         |> put_status(200)
-        |> render(:error, error: "Invalid user, user should be the address of wallet.") 
+        |> render(:error, error: "Invalid user, user should be the address of wallet.")
     end
   end
 
   defp is_valid_type_and_indices(index_type, indices) do
     index_list = String.split(indices, ",")
 
-    flag =
-      case index_type do
-        "contract" -> 
-          Enum.all?(index_list, fn x -> 
-            case Address.cast(x) do
-              {:ok, _} -> true
-              :error -> false
-            end
-          end)
+    check_list =
+      Enum.map(index_list, fn x ->
+        case index_type do
+          "contract" -> Address.cast(x)
+          "uuid" -> UUID.cast(x)
+        end
+      end)
 
-        "uuid" -> 
-          Enum.all?(index_list, fn x -> 
-            case UUID.cast(x) do
-              {:ok, _} -> true
-              :error -> false
-            end
-          end)
+    is_valid_type = index_type == "uuid" or index_type == "contract"
 
-        _ -> :error
-      end
+    is_valid_indices =
+      Enum.all?(check_list, fn x ->
+        case x do
+          {:ok, _} -> true
+          :error -> false
+        end
+      end)
 
-    case flag do
-      :error -> {:error, :invalid_type}
-      true -> {:ok, index_list}
-      false -> {:error, :invalid_indices}
+    cond do
+      not is_valid_type -> {:error, :invalid_type}
+      is_valid_indices -> {:ok, index_list}
+      true -> {:error, :invalid_indices}
     end
   end
 
   defp is_valid_user_address(user) do
-    case user do 
-      :error -> {:ok, nil}
-      
-      {:ok, user_address} -> 
+    case user do
+      :error ->
+        {:ok, nil}
+
+      {:ok, user_address} ->
         case Address.cast(user_address) do
           {:ok, hash} -> {:ok, hash}
           :error -> {:error, :invalid_user}
@@ -251,9 +250,9 @@ defmodule BlockScoutWeb.API.RPC.TokenController do
   defp fetch_contractaddress(params) do
     {:contractaddress_param, Map.fetch(params, "contractaddress")}
   end
- 
+
   defp fetch_type(params) do
-    {:index_type, Map.fetch(params, "type")} 
+    {:index_type, Map.fetch(params, "type")}
   end
 
   defp fetch_indices(params) do
