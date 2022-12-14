dd9cf51995969296b8f57e56566e88e873f98310
hundredark <hundredark@gmail.com>
2022-12-14 15:14:03 +0800

    Remove filter on api

--- apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/token_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/token_controller.ex
@@ -6,81 +6,6 @@ defmodule BlockScoutWeb.API.RPC.TokenController do
   alias Explorer.{Chain, PagingOptions}
   alias Explorer.Chain.Hash.Address
 
-  @filter_assets [
-    # USDT ERC20
-    "4d8c508b-91c5-375b-92b0-ee702ed2dac5",
-    # BOX
-    "f5ef6b5d-cc5a-3d90-b2c0-a2fd386e7a3c",
-    # IQ
-    "336d5d97-329c-330d-8e62-2b7c9ba40ea0",
-    # XIN
-    "c94ac88f-4671-3976-b60a-09064f1811e8",
-    # ONE
-    "2566bf58-c4de-3479-8c55-c137bb7fe2ae",
-    # EOS
-    "6cfe566e-4aad-470b-8c9a-2fd35b49c68d",
-    # pUSD
-    "31d2ea9c-95eb-3355-b65b-ba096853bc18",
-    # MOB
-    "eea900a8-b327-488c-8d8d-1428702fe240",
-    # USDC
-    "9b180ab6-6abe-3dc0-a13f-04169eb34bfa",
-    # DOGE
-    "6770a1e5-6086-44d5-b60f-545f9d9e8ffd",
-    # UNI
-    "a31e847e-ca87-3162-b4d1-322bc552e831",
-    # CNB
-    "965e5c6e-434c-3fa9-b780-c50f43cd955c",
-    # ETH
-    "43d61dcd-e413-450d-80b8-101d5e903357",
-    # SHIB
-    "dcde18b9-f015-326f-b8b1-5b820a060e44",
-    # AKITA
-    "aa189c4c-99ca-39eb-8d96-71a8f6f7218a",
-    # RUM
-    "4f2ec12c-22f4-3a9e-b757-c84b6415ea8f",
-    # FIL
-    "08285081-e1d8-4be6-9edc-e203afa932da",
-    # AKT
-    "9c612618-ca59-4583-af34-be9482f5002d",
-    # MANA
-    "c3dc19ae-d087-3279-ac51-dc655940256a",
-    # EURT
-    "2f5bef0e-d41a-3cf3-b6fa-b8dd0d8a3327",
-    # PINK
-    "14693c1a-d835-3572-b9b4-e0cbb62099e5",
-    # DOT
-    "54c61a72-b982-4034-a556-0d99e3c21e39",
-    # TRX
-    "25dabac5-056a-48ff-b9f9-f67395dc407c",
-    # ALGO
-    "706b6f84-3333-4e55-8e89-275e71ce9803",
-    # XLM
-    "56e63c06-b506-4ec5-885a-4a5ac17b83c1",
-    # BTC
-    "c6d0c728-2624-429b-8e0d-d9d19b6592fa",
-    # LINK
-    "f6f1c01c-8489-3346-b127-dc0dc09b9ce7",
-    # XRP
-    "23dfb5a5-5d7b-48b6-905f-3970e3176e27",
-    # MATIC
-    "9682b8e9-6f16-3729-b07b-bc3bc56e5d79",
-    # USDT TRC20
-    "b91e18ff-a9ae-3dc7-8679-e935d9a4b34b",
-    # CKB
-    "d243386e-6d84-42e6-be03-175be17bf275",
-    # LTC
-    "76c802a2-7c88-447f-a93e-c29c9e5dd9c8",
-    # SOL
-    "64692c23-8971-4cf4-84a7-4dd1271dd887",
-    # BNB, Binance
-    "17f78d7c-ed96-40ff-980c-5dc62fecbc85",
-    # mAED
-    "b5289c48-ec3a-3cdb-b2c4-0913d1812cd5",
-    # BNB, Binance Smart Chain
-    "1949e683-6a08-49e2-b087-d6b72398588f"
-  ]
-
   def gettoken(conn, params) do
     with {:contractaddress_param, {:ok, contractaddress_param}} <- fetch_contractaddress(params),
          {:format, {:ok, address_hash}} <- to_address_hash(contractaddress_param),
@@ -132,7 +57,7 @@ defmodule BlockScoutWeb.API.RPC.TokenController do
 
     erc20_assets =
       Enum.filter(total_assets, fn x ->
-        x.type == "ERC-20" and not is_nil(x.mixin_asset_id) and Enum.member?(@filter_assets, x.mixin_asset_id)
+        x.type == "ERC-20" and not is_nil(x.mixin_asset_id)
       end)
 
     asset_list =
@@ -150,7 +75,7 @@ defmodule BlockScoutWeb.API.RPC.TokenController do
 
     erc20_assets =
       Enum.filter(res, fn x ->
-        x.type == "ERC-20" and not is_nil(x.mixin_asset_id) and Enum.member?(@filter_assets, x.mixin_asset_id)
+        x.type == "ERC-20" and not is_nil(x.mixin_asset_id)
       end)
 
     asset_list =
